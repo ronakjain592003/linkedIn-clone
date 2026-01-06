c80344c273d550e9ee5eec56a52fec7467f830a5
Ronak Jain <rj9496760@gmail.com>
2026-01-06 15:40:23 +0530

    Describe your changes here

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import upload from "../middlewares/multer.js"
-import { comment, createPost, getPost, like } from "../controllers/post.Controllers.js"
+import { comment, createPost, getPost, like } from "../controllers/post.controllers.js"
 const postRouter=express.Router()
 
 postRouter.post("/create",isAuth,upload.single("image"),createPost)
